fad6701bca9a1ca9503e6a7c13347299920c442d
takamasa_kan <takamasa_kan@fandmnet.com>
2019-11-12 11:22:41 +0900

    seo変更

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="大阪在住WEBプログラマーのポートフォリオです。">
 
-        <title>Laravel</title>
+        <title>ポートフォリオ | kankan</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
